6240e0f39bb842919c229887325a90ecd3580ac2
chisambwe <c.m.kapihya@gmail.com>
2022-01-11 14:59:59 +0000

    05 create first

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ https://miro.com/app/board/uXjVOWooC-c=/
 a Controller
 a Bookmark class
 A bookmark view
+
+1) Connect to psql
+2) Create the database using the psql command CREATE DATABASE bookmark_manager;
+3) Connect to the database using the pqsl command \c bookmark_manager;
+4) Run the query we have saved in the file 01_create_bookmarks_table.sql
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,6 +1,20 @@
+# As a time-pressed user
+# So that I can quickly go to web sites I regularly visit
+# I would like to see a list of bookmarks
+
 feature 'Viewing bookmarks' do
     scenario 'visiting the index page' do
       visit('/')
       expect(page).to have_content "Bookmark Manager"
+    end
+  end
+
+  feature 'Viewing bookmarks' do
+    scenario 'A user can see bookmarks' do
+      visit('/bookmarks')
+  
+      expect(page).to have_content "http://www.google.com"
+      expect(page).to have_content "http://www.ups.com"
+      expect(page).to have_content "http://www.bbc.co.uk"
     end
   end
